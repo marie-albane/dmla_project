b160123b3fd6c228502089abcd813c386d31134b
stephane portier <steph.portier3@gmail.com>
2024-12-02 15:56:08 +0100

    ajout des images de traitement intermediaire + preproc associé

--- dmla/interface/predict.py
+++ dmla/interface/predict.py
@@ -10,13 +10,13 @@ import matplotlib.pyplot as plt
 def predict() :
 
     # Charger une image aléatoire preprocessed
-    image_test, image_name = load_and_process_random_image(wanted_dataset = "testing")
-    image_with_batch = np.expand_dims(image_test, axis=0)
+    image_rgb, cropped_image, resized_image, normalized_image, image_name = load_and_process_random_image(wanted_dataset = "testing")
+    image_with_batch = np.expand_dims(normalized_image, axis=0)
     print("✅ => image chargée")
 
 
     #Charger le model avec la fonction best_model = load_model() et l image
-    best_model = load_model()
+    best_model, model_number = load_model()
     print("✅ => modèle chargé")
 
     result = best_model.predict(image_with_batch)
@@ -34,7 +34,27 @@ def predict() :
         print(f"⚠️ Vous présentez les symptômes d'une DMLA\n nous allons programmer un prochain rendez-vous spécialiste")
 
     print("\nPour l'image:",image_name)
-    plt.imshow(image_test)
+    print("basé sur le model: " + model_number)
+
+    plt.figure(figsize=(13,5))
+    # First subplot
+    plt.subplot(1,4,1)
+    plt.imshow(image_rgb)
+    plt.title('image originale')
+    # Second subplot
+    plt.subplot(1,4,2)
+    plt.imshow(cropped_image)
+    plt.title('image croppée (réduction du noir sur les cotés)')
+    # Global figure methods
+    plt.subplot(1,4,3)
+    plt.imshow(resized_image)
+    plt.title('image resizée à '+str(TARGETED_IMAGES_X)+'x'+str(TARGETED_IMAGES_Y))
+
+    plt.subplot(1,4,4)
+    plt.imshow(normalized_image)
+    plt.title('image finale (div des couleurs par 255)')
+    plt.show()
+
     plt.show()
 
 if __name__ == '__main__':
