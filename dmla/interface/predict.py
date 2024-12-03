6f9b8e444f11532450fdad629a785d35173f643b
michel desportes <desportesm@gmail.com>
2024-12-03 15:53:20 +0100

    Vérification de la DMLA dans CSV git add dmla/interface/predict.py

--- dmla/interface/predict.py
+++ dmla/interface/predict.py
@@ -5,6 +5,10 @@ from tensorflow import keras
 from keras import Model
 from dmla.ml_logic.preprocessor import load_and_process_random_image
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from pathlib import Path
 
 #faire best_model.predict(image)
 def predict() :
@@ -13,6 +17,27 @@ def predict() :
     image_rgb, cropped_image, resized_image, normalized_image, image_name = load_and_process_random_image(wanted_dataset = "testing")
     image_with_batch = np.expand_dims(normalized_image, axis=0)
     print("✅ => image chargée")
+
+    #Récupérer le y_true
+    images_path = os.path.join(DATA_PATH, "raw_data", "testing")
+    path_csv = os.path.join(DATA_PATH, "raw_data", f"RFMiD_Testing_Labels.csv")
+
+    # Load data
+    try:
+        data = pd.read_csv(path_csv)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"CSV file not found at path: {path_csv}")
+
+    # Set index and extract ARMD column
+    if "ID" not in data.columns or "ARMD" not in data.columns:
+        raise ValueError("The required columns ('ID', 'ARMD') are missing from the CSV file.")
+
+    print(f"image name est {image_name}")
+    # Retirer l'extension .png ou autre
+    image_base_name = os.path.splitext(image_name)[0]
+    data = data.set_index("ID")
+    image_index = int(image_base_name) + 1
+    y_true = data["ARMD"][image_index]
 
 
     #Charger le model avec la fonction best_model = load_model() et l image
@@ -35,6 +60,11 @@ def predict() :
 
     print("\nPour l'image:",image_name)
     print("basé sur le model: " + model_number)
+
+    if y_true == 1 :
+        print(f"Confirmation avec le fichier csv pour l'image {image_base_name} : Présence de  DMLA ⚠️")
+    else :
+        print(f"Confirmation avec le fichier csv pour l'image {image_base_name}: Absence DMLA✅")
 
     plt.figure(figsize=(13,5))
     # First subplot
